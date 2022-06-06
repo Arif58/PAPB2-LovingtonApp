03d9412aad6ea7a35fa1a12523f982928da83850
notSteampunks <isnanda.a@mail.ugm.ac.id>
2022-06-06 18:41:32 +0700

    add notification (not fix)

--- app/src/main/java/com/example/lovingtonapp/MainActivity.java
+++ app/src/main/java/com/example/lovingtonapp/MainActivity.java
@@ -1,14 +1,57 @@
 package com.example.lovingtonapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
 import android.os.Bundle;
+import android.view.View;
+
+
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.graphics.Color;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button tombol;
+    String channelnotif = "channelku" ;
+    String channelid = "default" ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tombol = (Button)findViewById(R.id.buttonNotification);
+        tombol.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                notif();
+
+            }
+        });
     }
+
+    private void notif() {
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(MainActivity. this, channelid )
+                .setSmallIcon(R.drawable.group)
+                .setContentTitle( "Your order's confirmed" )
+                .setContentText( "Check your Whatsapp, we will texting soon!" );
+        NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context. NOTIFICATION_SERVICE ) ;
+        if (android.os.Build.VERSION. SDK_INT >= android.os.Build.VERSION_CODES. O ) {
+            int importance = NotificationManager. IMPORTANCE_HIGH ;
+            NotificationChannel notificationChannel = new
+                    NotificationChannel( channelnotif , "contoh channel" , importance) ;
+            notificationChannel.enableLights( true ) ;
+            notificationChannel.setLightColor(Color. RED ) ;
+            mBuilder.setChannelId( channelnotif ) ;
+            assert mNotificationManager != null;
+            mNotificationManager.createNotificationChannel(notificationChannel) ;
+        }
+        assert mNotificationManager != null;
+        mNotificationManager.notify(( int ) System. currentTimeMillis (), mBuilder.build()) ;
+    }
+
 }
